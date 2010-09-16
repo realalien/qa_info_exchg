54c319da712f6260f3f380d5a8c3e25882793863
Zhu Jia Cheng <realalien@gmail.com>
2010-09-16 18:52:10 +0800

    add a script for learning p4ruby

---
+++ sandbox/p4ruby_api_usage.rb
@@ -0,0 +1,11 @@
+
+
+# Please setup a perforce server / client to manipulate the 
+# Please install p4 ruby API from, 
+
+# SUG: avoid using hard-coded string 
+# SUG: make functions, reusable module rather than stand alone scripts.
+
+
+
+
