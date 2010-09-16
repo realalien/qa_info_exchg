8add5583fdc18ded19d97818ba807dd12a5a8350
Zhu Jia Cheng <realalien@gmail.com>
2010-09-16 17:42:12 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QaInfoExchg::Application.config.secret_token = '3d67d39640c6ddca7c8bb7695d5584b6cd694dc1c6865b169dd27cbad44c879c90e4d44d6870540c957097b576be49ce529fa1fbbb87b6ccde17e4ba5f2fea73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+QaInfoExchg::Application.config.session_store :cookie_store, :key => '_qa_info_exchg_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# QaInfoExchg::Application.config.session_store :active_record_store
